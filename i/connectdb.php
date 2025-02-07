cda54b6b1f723cb5692b34900afe2741e409dea2
numxiso <phonthida.nt@gmail.com>
2025-02-07 17:45:41 +0700

    i

--- i/connectdb.php
+++ i/connectdb.php
@@ -1,8 +1,8 @@
 <?php 
 $host = "localhost";
 $user = "root";
-$pwd = "147852369";
-$db = "shop4604";
+$pwd = "12345678Y";
+$db = "shop4605";
 
 $conn = mysqli_connect ($host,$user,$pwd) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
 mysqli_select_db($conn, $db)or die ("เลือกฐานข้อมูลไม่ได้");
